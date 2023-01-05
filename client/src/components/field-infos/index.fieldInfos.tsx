8ec27c8f6d8b98f84e6d7c2b42ab9cd5b23bee7b
Yago Fogaça <yagofogaca20@gmail.com>
2023-01-05 19:18:46 -0300

    feat: criação da seção de infos

---
+++ client/src/components/field-infos/index.fieldInfos.tsx
@@ -0,0 +1,12 @@
+import './fieldInfos.style.css';
+
+export function FieldInfos() {
+  return (
+    <>
+      <div className="field-infos">
+        <h3 id="title-name">Yago</h3>
+        <span id="trophies">35</span>
+      </div>
+    </>
+  );
+}
