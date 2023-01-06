82ea5336c101bbbc54c14b3d1f3ae861cc300e91
Yago Fogaça <yagofogaca20@gmail.com>
2023-01-06 18:59:54 -0300

    feat: página do jogo

--- client/src/page/game/index.game.tsx
+++ client/src/page/game/index.game.tsx
@@ -1,18 +1,45 @@
+import { useEffect, useState } from 'react';
+import { FieldInfos } from '../../components/field-infos/index.fieldInfos';
+import { Form } from '../../components/form/index.form';
+import { Game } from '../../components/game/index.game';
+import { Lyrics } from '../../components/lyrics-section/index.lyrics';
+import { IWords } from '../../utils/interfaces/mocks';
+import { wordGenerator } from '../../utils/word-generator/wordGenerator';
 import './game.style.css';
 
 export function PageGame() {
+  const word = 'hipopótamo';
+  const wordArray = word.split('');
+  const [wordTeste, setWordTeste] = useState<IWords>({ topic: '', word: '' });
+
+  const [disabledInput, setDisabledInput] = useState(false);
+  const [wordCopy, setWordCopy] = useState<number[]>(
+    new Array(wordArray.length).fill(0),
+  );
+
+  function Acerto(correct: string) {
+    const wordCopyT: number[] = wordCopy;
+    wordArray.map((lyrics, index) => {
+      if (lyrics === correct) {
+        wordCopyT[index] = 1;
+        setWordCopy(wordCopyT);
+      }
+    });
+  }
+
+  function startGame() {
+    const word = wordGenerator();
+    console.log(word);
+    setWordTeste(word);
+  }
+
+  useEffect(() => {
+    startGame();
+  }, []);
+
   return (
     <section id="section-game">
-      <div className="field-infos">
-        <h3 id="title-name">Yago</h3>
-        <span id="trophies">35</span>
-      </div>
-
-      <div className="field-game">
-        <div className="field-tema">
-          <h2 id="title-tema">Animal</h2>
-        </div>
-      </div>
+      <Game />
     </section>
   );
 }
