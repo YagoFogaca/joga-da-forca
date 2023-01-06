196283cf818fc00bcd6b391c64ea7b37f3329b68
Yago Fogaça <yagofogaca20@gmail.com>
2023-01-06 18:59:24 -0300

    feat: criação dos dados mocados

---
+++ client/src/mocks/words.ts
@@ -0,0 +1,20 @@
+import { IWords } from '../utils/interfaces/mocks';
+
+export const words: IWords[] = [
+  {
+    topic: 'Animal',
+    word: 'hipopótamo',
+  },
+  {
+    topic: 'Animal',
+    word: 'Leão',
+  },
+  {
+    topic: 'Animal',
+    word: 'Cachorro',
+  },
+  {
+    topic: 'Objeto',
+    word: 'Caneta',
+  },
+];
